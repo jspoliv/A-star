739be565518c589c00758f104d9b21fd5443fbc3
jspol <jspoliv@gmail.com>
2023-10-01 20:28:42 -0300

    minor changes and some comments

--- main.c
+++ main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "linked_list.h"
+#include "list.h"
 
 #define infinity 10000
 #define numNeighbors 4
-#define file1 "entrada300.txt"
+#define file1 "entrada100.txt"
 #define file2 "saida.txt"
 
 int reconstruct_path(int came_from[], int current_node, char map_in[], char map_out[]);
-int dist_between(int neighbor, char map_in[]);
+int dist_between(char map_weight);
 void neighbor_nodes(int neighbor[], int current, int n);
 int lowest_f_score(node **openset, int f_score[]);
 int h_cost(int current, int goal, int n);
-void Load(FILE *in, char map_in[], char map_out[], node **openset, int closedset[], int came_from[], int g_score[], int f_score[], int *goal, int* start);
+void load(FILE *in, char map_in[], char map_out[], node **openset, int closedset[], int came_from[], int g_score[], int f_score[], int *goal, int* start);
 
 int main(){
-    FILE *in = fopen(file1, "r"), *out = fopen(file2, "w");
+    FILE *in = fopen(file1, "r"), *out;
     int n, n2, i, start, goal, current, tentative_g_score, neighbor[numNeighbors];
     fscanf(in, "%d", &n);
     n2 = n*n;
     printf("n:%i\nn*n:%i\n\n", n, n2);
-    int *closedset = (int*)malloc(n2*sizeof(int)), *came_from = (int*)malloc(n2*sizeof(int));
+    int *closedset = (int*)calloc(n2, sizeof(int)), *came_from = (int*)malloc(n2*sizeof(int));
     int *g_score = (int*)malloc(n2*sizeof(int)), *f_score = (int*)malloc(n2*sizeof(int));
     char *map_in = (char*)malloc(n2*sizeof(char)), *map_out = (char*)malloc(n2*sizeof(char));
     node *openset = NULL;
@@ -33,21 +33,21 @@ int main(){
     // file2 & out: output file.
     // n: X axis value of the map, as the X axis and Y axis have the same value, you can get it only once.
     // n2: n squared
-    // start: start position's location.
-    // goal: goal position's location.
+    // start: start position coordinate.
+    // goal: goal position coordinate.
     // current: position of the lowest_f_score()
     // openset: the set of tentative nodes to be evaluated, initially containing the start node.
     // closedset: the set of nodes already evaluated.
     // came_from: the map of navigated nodes.
     // g_score: cost from start along best known path.
-    // tentative_g_score: g_score[current] + dist_between(neighbor[i], map_in)
-    // f_score: estimated total cost from start to goal through y.
+    // tentative_g_score: g_score[current] + dist_between(map_in[neighbor[i]])
+    // f_score: g_score + heuristic cost
     // neighbor: array with the current neighbors' positions.
     // map_in: original map.
     // map_out: final map containing the cost and the way to the exit.
     */
 
-    Load(in, map_in, map_out, &openset, closedset, came_from, g_score, f_score, &goal, &start);
+    load(in, map_in, map_out, &openset, closedset, came_from, g_score, f_score, &goal, &start);
 
     g_score[start] = 0;
     f_score[start] = g_score[start] + h_cost(start, goal, n);
@@ -55,7 +55,8 @@ int main(){
     printf("Main while() start...\n\n");
     while(openset!=NULL){
         current = lowest_f_score(&openset, f_score);
-        if(current == goal){ // execute and end program
+        if(current == goal){ // goal found, execute and exit code
+            out = fopen(file2, "w");
             printf("Reconstruct start... ");
             fprintf(out, "%d", reconstruct_path(came_from, goal, map_in, map_out));
             printf("ended sucessfully.\n\n");
@@ -77,7 +78,7 @@ int main(){
         neighbor_nodes(neighbor, current, n); // fetches position of current neighbors.
         for(i=0; i<numNeighbors; i++){
             if(neighbor[i]!=-1){ // neighbor == -1 is outside the map_in[] scope
-                tentative_g_score = g_score[current] + dist_between(neighbor[i], map_in);
+                tentative_g_score = g_score[current] + dist_between(map_in[neighbor[i]]);
                 if(closedset[neighbor[i]]==1 && tentative_g_score>=g_score[neighbor[i]]){
                     continue;
                 }
@@ -96,22 +97,22 @@ int main(){
 }
 
 
-/* function Load
+/* function load()
 // parameters: input file, map_in, map_out, openset, closedset, came_from, g_score, f_score, goal, start, n
 //
 // The function starts all used variables.
 */
-void Load(FILE *in, char map_in[], char map_out[], node **openset, int closedset[], int came_from[], int g_score[], int f_score[], int *goal, int* start){
+void load(FILE *in, char map_in[], char map_out[], node **openset, int closedset[], int came_from[], int g_score[], int f_score[], int *goal, int* start){
     printf("load start... ");
     int i=0;
     while(!feof(in)){// Load map.
         fscanf(in, "%c", &map_in[i]);
         if(map_in[i]=='X' || map_in[i]=='O' || map_in[i]=='V' || map_in[i]=='W' || map_in[i]=='#'){ // Accepted char set
             // Set other maps.
-            closedset[i]=0;
+            // closedset[i]=0; // initialized by calloc
             came_from[i]=-1;
-            g_score[i]=10000000; // arbitrarily high number that will be swapped
-            f_score[i]=10000000; // arbitrarily high number that will be swapped
+            g_score[i]=10000000; // arbitrarily high number that will be swapped.
+            f_score[i]=10000000; // arbitrarily high number that will be swapped.
             map_out[i]=map_in[i];
 
             switch(map_in[i]){
@@ -120,16 +121,9 @@ void Load(FILE *in, char map_in[], char map_out[], node **openset, int closedset
                     break;
                 case 'O':
                     *start = i;
-                    addLast(&(*openset), i); // Set start position to 1 in openset.
+                    addLast(&(*openset), i); // Mark the start position as open.
                     break;
             }
-            /* same as the switch above
-            if(map_in[i]=='X'){
-                *goal = i;
-            } else if(map_in[i]=='O'){
-                *start = i;
-                openset[i]=1; // Set start position to 1 in openset.
-            }*/
             i++;
         }
     }// End Load Map.
@@ -139,7 +133,7 @@ void Load(FILE *in, char map_in[], char map_out[], node **openset, int closedset
 } // Function end.
 
 
-/* function recontruct_path
+/* function recontruct_path()
 // parameters: came_from vector, current position, map, final map.
 //
 // The function gets the best path from the start to the exit of the map and print it in the final map.
@@ -153,25 +147,26 @@ int reconstruct_path(int came_from[], int current_node, char map_in[], char map_
             map_out[current_node]='*';
         }
         sum = reconstruct_path(came_from, came_from[current_node], map_in, map_out);
-        return(sum + dist_between(current_node, map_in));
+        return(sum + dist_between(map_in[current_node]));
     } else {
         return 0;
     }
 }
 
-/* function dist_between
-// parameters: position of the neighbor, map of characters.
+/* function dist_between()
+// parameters: map_in[position].
 //
-// The function calculates the  value of the neighbor based on what that neighbor is.
-// Returns the respective value of the neighbor.
+// Maps a char input to return an int.
 */
-int dist_between(int neighbor, char map_in[]){
-    if(map_in[neighbor]=='V' || map_in[neighbor]=='X'){
-        return 1;
-    } else if(map_in[neighbor]=='W'){
-        return 2;
-    } else/*if(map_in[neighbor]=='#' || map_in[neighbor]=='O')*/{
-        return infinity;
+int dist_between(char map_weight){ // map_in[neighbor]
+    switch(map_weight){
+        case 'X':
+        case 'V':
+            return 1;
+        case 'W':
+            return 2;
+        default: // case: '#' || 'O'
+            return infinity;
     }
 }
 
@@ -199,13 +194,12 @@ void neighbor_nodes(int neighbor[], int current, int n){
 }
 
 
-/* function h_cost
+/* function h_cost()
 // parameters: current position, goal position, size of the X axis of the map.
 //
-// The function calculates the heuristic cost from the current to the goal position.
-// Returns the heuristic cost from the current to the goal position.
+// Returns the heuristic cost from position "current" to "goal".
 */
-int h_cost(int current, int goal, int n){ // Manhattan distance frim [current] to [goal]
+int h_cost(int current, int goal, int n){ // Manhattan distance from [current] to [goal]
     return(abs((goal/n)-(current/n))+abs((goal%n)-(current%n)));
     /* extended version of the return() above.
     int cx, cy, gx, gy; // transforms a position in a N^2 sized array into a [x,y] pair in a NxN matrix
